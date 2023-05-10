51dddb768089d39e6077aafb24b75777f1e7108e
Reuben Miller <reuben.d.miller@gmail.com>
2023-05-10 14:21:43 +0200

    feat(ssh): enable sshd service by default during bootstrapping
    Signed-off-by: Reuben Miller <reuben.d.miller@gmail.com>

--- images/common/config/bootstrap/post.d/10-start-services.sh
+++ images/common/config/bootstrap/post.d/10-start-services.sh
@@ -5,9 +5,18 @@
 
 set -e
 
-if command -v systemctl >/dev/null 2>&1; then
-    sleep 5
-    echo "Enabling/starting tedge-container-monitor"
-    systemctl enable tedge-container-monitor
-    systemctl start tedge-container-monitor
-fi
+start_enable_service() {
+    name="$1"
+    if command -v systemctl >/dev/null 2>&1; then
+        echo "Enabling/starting $name"
+        sudo systemctl enable "$name"
+
+        if [ -d /run/systemd/system ]; then
+            sudo systemctl start "$name"
+        fi
+    fi
+}
+
+sleep 5
+start_enable_service "tedge-container-monitor"
+start_enable_service "ssh"
