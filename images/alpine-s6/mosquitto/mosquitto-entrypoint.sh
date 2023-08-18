b16a2ba786a9c1a3cc5a955ce69ba1ecadd0ed43
Reuben Miller <reuben.d.miller@gmail.com>
2023-08-18 18:07:24 +0200

    remove unused files
    Signed-off-by: Reuben Miller <reuben.d.miller@gmail.com>

--- images/alpine-s6/mosquitto/mosquitto-entrypoint.sh
+++ images/alpine-s6/mosquitto/mosquitto-entrypoint.sh
@@ -35,32 +35,6 @@ while true; do
     if echo "$resp" | grep -q "Saving configuration for requested bridge"; then
         break
     fi
-
-    # Option 2: use mosquitto_pub
-    # device_id=$(tedge config get device.id 2>/dev/null)
-    # device_type=$(tedge config get device.type 2>/dev/null)
-    # device_key=$(tedge config get device.key_path 2>/dev/null)
-    # device_cert=$(tedge config get device.cert_path 2>/dev/null)
-    # device_ca_path=$(tedge config get c8y.root_cert_path 2>/dev/null)
-
-    # ca_options=""
-    # if [ -d "$device_ca_path" ]; then
-    #     ca_options="--capath $device_ca_path"
-    # elif [ -f "$device_ca_path" ]; then
-    #     ca_options="--cafile $device_ca_path"
-    # fi
-
-    # if mosquitto_pub \
-    #     -h "$TEDGE_C8Y_URL" \
-    #     -p "$C8Y_MQTT_PORT" \
-    #     --key "$device_key" \
-    #     --cert "$device_cert" \
-    #     $ca_options \
-    #     --id "$device_id" \
-    #     -t "s/us" \
-    #     -m "100,$device_id,$device_type"; then
-    #     break
-    # fi
     sleep 10
 done
 
@@ -71,7 +45,6 @@ user="$(id -u)"
 if [ "$user" = '0' ]; then
 	if [ -d "/mosquitto" ]; then
         # Use the gid and uid instead of the name
-        # chown -R mosquitto:mosquitto /mosquitto || true
         chown -R "1883:1883" /mosquitto || true
     fi
 	if [ -d "/etc/tedge/device-certs/" ]; then
