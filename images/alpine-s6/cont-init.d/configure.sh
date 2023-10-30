d85422ed24b9b86c979484d0e20134c536d34983
Reuben Miller <reuben.d.miller@gmail.com>
2023-10-31 00:17:15 +0100

    add workaround for https://github.com/thin-edge/thin-edge.io/issues/2389
    Signed-off-by: Reuben Miller <reuben.d.miller@gmail.com>

--- images/alpine-s6/cont-init.d/configure.sh
+++ images/alpine-s6/cont-init.d/configure.sh
@@ -7,3 +7,21 @@ if [ -n "$EXTERNAL_ID" ]; then
     tedge config set http.bind.address "$EXTERNAL_ID"
     tedge config set c8y.proxy.bind.address "$EXTERNAL_ID"
 fi
+
+# register device
+TOPIC_ROOT=$(tedge config get mqtt.topic_root)
+TOPIC_ID=$(tedge config get mqtt.device_topic_id)
+
+# FIXME: Remove once https://github.com/thin-edge/thin-edge.io/issues/2389 is resolved
+# A manual registration before the service starts up seems to prevent duplicate registration messages
+case "$TOPIC_ID" in
+    device/main//)
+        ;;
+    *)
+        echo "manually registering child-device" >&2
+        name=$(echo "$TOPIC_ID" | cut -d/ -f2)
+        body=$(printf '{"@type":"child-device","name":"%s"}' "$name")
+        tedge mqtt pub -r "$TOPIC_ROOT/$TOPIC_ID" "$body"
+        sleep 1
+        ;;
+esac
