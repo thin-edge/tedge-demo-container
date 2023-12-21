a63419d8a80307eae3c203b719844cee89df0ee8
Reuben Miller <reuben.d.miller@gmail.com>
2023-12-21 01:27:43 +0100

    add tedge user to the adm group
    The adm group grants the tedge user access to monitoring of admin related files, like apt etc.
    
    Signed-off-by: Reuben Miller <reuben.d.miller@gmail.com>

--- images/common/bootstrap.sh
+++ images/common/bootstrap.sh
@@ -403,6 +403,9 @@ post_configure() {
         sudo sh -c "echo '%sudo ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/all"
     fi
 
+    echo "Add tedge to the admin group to give it access to monitoring files"
+    usermod -a -G adm tedge ||:
+
     if [ ! -f /etc/sudoers.d/tedge ]; then
         sudo sh -c "echo 'tedge  ALL = (ALL) NOPASSWD: /usr/bin/tedge, /usr/bin/tedge-write /etc/*, /etc/tedge/sm-plugins/[a-zA-Z0-9]*, /bin/sync, /sbin/init, /bin/systemctl, /bin/journalctl, /sbin/shutdown, /usr/bin/on_shutdown.sh' > /etc/sudoers.d/tedge"
     fi
